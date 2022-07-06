bb8cfbcf8ee0d62cd1750d0b220a174b584c364b
Andrés Perdomo <alvarado0925@gmail.com>
2022-07-06 17:08:46 -0500

    Variable reactivas con ref y reactive

--- src/components/CompositionApi.vue
+++ src/components/CompositionApi.vue
@@ -1,15 +1,47 @@
 <template>
   <h1>Composition API</h1>
+  <p>{{ text }} - Contador: {{ counter }}</p>
+  <p>
+    <b>Nombre:</b>
+    {{ persona.name }}
+    <b>Edad:</b>
+    {{ persona.age }}
+  </p>
+  <input type="text" v-model="persona.name" />
 </template>
 
 <script>
-import { onMounted } from "vue";
+import { ref, reactive, onMounted } from "vue";
 
 export default {
   setup() {
+    // Ref
+    // Sirve para agregar reactividad a tipos de datos primitivos
+    // Ejmeplo: String, Number, Boolean, etc
+    const text = ref("Hola vue");
+    const counter = ref(0);
+
+    // Reactive
+    // Sirve para agregar reactividad a objetos
+    const persona = reactive({
+      name: "Felipe",
+      age: 20,
+    });
+
+    setInterval(() => {
+      counter.value++;
+    }, 500);
+
     onMounted(() => {
+      counter.value = 100;
       console.log("mounted Composition API");
     });
+
+    return {
+      text,
+      counter,
+      persona,
+    };
   },
 };
 </script>
