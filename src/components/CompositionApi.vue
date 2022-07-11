a7bc2738fde005f31c8369b0938912e19aef5aaa
Andrés Perdomo <alvarado0925@gmail.com>
2022-07-11 11:55:52 -0500

    Migrando script a script setup en Composition API

--- src/components/CompositionApi.vue
+++ src/components/CompositionApi.vue
@@ -21,114 +21,118 @@
   <input type="text" v-model="persona.apellido" />
 </template>
 
-<script>
-import { ref, toRefs, reactive, onMounted, watch, computed, inject } from "vue";
+<script setup>
+import {
+  defineProps,
+  defineExpose,
+  useSlots,
+  useAttrs,
+  ref,
+  toRefs,
+  reactive,
+  onMounted,
+  watch,
+  computed,
+  inject,
+} from "vue";
 
-export default {
-  props: {
-    ciudad: String,
-    continente: String,
-  },
-  setup(props, { attrs, expose, slots }) {
-    /*
-     * Context - attrs
-     * Atributos pasados al componente, que no son recibidos en props
-     */
-    console.log("Attrs =>", attrs.otro);
+const props = defineProps({
+  ciudad: String,
+  continente: String,
+});
 
-    /**
-     * Context - Solts
-     * Un objeto con todas las funciones de renderizado de los templates de Slots
-     */
-    console.log("Slots =>", slots);
+/*
+ * Context - attrs
+ * Atributos pasados al componente, que no son recibidos en props
+ */
+const attrs = useAttrs();
+console.log("Attrs =>", attrs.otro);
 
-    /*
-     * toRefs
-     * Convierte todas las propiedades en un objeto simple con propiedades que son referencias.
-     * Con esto se le puede dar reactividad todos los props, ya que estos no lo son, en caso de
-     * que se necesite
-     */
-    const { ciudad, continente } = toRefs(props);
+/**
+ * Context - Solts
+ * Un objeto con todas las funciones de renderizado de los templates de Slots
+ */
+const slots = useSlots();
+console.log("Slots =>", slots);
 
-    /*
-     * toRef
-     * Hace lo mismo que toRefs, pero enfocado a una sola propiedad del objeto
-     *
-     * const ciudad = toRef(props, "ciudad");
-     * const continente = toRef(props, "continente");
-     *
-     */
+/*
+ * toRefs
+ * Convierte todas las propiedades en un objeto simple con propiedades que son referencias.
+ * Con esto se le puede dar reactividad todos los props, ya que estos no lo son, en caso de
+ * que se necesite
+ */
+const { ciudad, continente } = toRefs(props);
 
-    // Refs
-    // Sirve para agregar reactividad a tipos de datos primitivos
-    // Ejemplo: String, Number, Boolean, etc
-    const text = ref("Hola vue");
-    const counter = ref(0);
-    const casa = ref("🐺Stark");
-    /*
-     * Los props también se podrían volver reactivos usando ref, pero serían más repetitivos
-     *
-     * const ciudad = ref(props.ciudad);
-     * const continente = ref(props.continente);
-     */
+/*
+ * toRef
+ * Hace lo mismo que toRefs, pero enfocado a una sola propiedad del objeto
+ *
+ * const ciudad = toRef(props, "ciudad");
+ * const continente = toRef(props, "continente");
+ *
+ */
 
-    // Los Template Refs se deben declarar siempre como null
-    const inputUsername = ref(null);
+// Refs
+// Sirve para agregar reactividad a tipos de datos primitivos
+// Ejemplo: String, Number, Boolean, etc
+const text = ref("Hola vue");
+const counter = ref(0);
+const casa = ref("🐺Stark");
+/*
+ * Los props también se podrían volver reactivos usando ref, pero serían más repetitivos
+ *
+ * const ciudad = ref(props.ciudad);
+ * const continente = ref(props.continente);
+ */
 
-    const username = inject("username");
+// Los Template Refs se deben declarar siempre como null
+const inputUsername = ref(null);
 
-    // Reactive
-    // Sirve para agregar reactividad a objetos
-    const persona = reactive({
-      nombre: "Felipe",
-      apellido: "Perez",
-      edad: 20,
-      nombreCompleto: computed(() => {
-        return `${persona.nombre} ${persona.apellido}`;
-      }),
-    });
+const username = inject("username");
 
-    setInterval(() => {
-      counter.value++;
-    }, 5000);
+// Reactive
+// Sirve para agregar reactividad a objetos
+const persona = reactive({
+  nombre: "Felipe",
+  apellido: "Perez",
+  edad: 20,
+  nombreCompleto: computed(() => {
+    return `${persona.nombre} ${persona.apellido}`;
+  }),
+});
 
-    // Watchers
-    // Con refs
-    watch(counter, (actual, anterior) => {
-      console.log(actual, anterior);
-    });
-    // Con reactive
-    watch(
-      () => persona.nombre,
-      (actual, anterior) => {
-        console.log("Nombre actual:", actual, "Nombre anterior:", anterior);
-      }
-    );
+setInterval(() => {
+  counter.value++;
+}, 5000);
 
-    // Hooks
-    onMounted(() => {
-      inputUsername.value.focus();
-      counter.value = 100;
-      console.log("mounted Composition API");
-    });
+// Computed props
+const nombreCompletoMayus = computed(() => {
+  return `${persona.nombre} ${persona.apellido} de la casa ${casa.value} ${ciudad.value} (${continente.value})`.toUpperCase();
+});
 
-    // Context - Expose. Puede publicar una variable o un objeto
-    expose({
-      persona,
-      counter,
-    });
+// Watchers
+// Con refs
+watch(counter, (actual, anterior) => {
+  console.log(actual, anterior);
+});
+// Con reactive
+watch(
+  () => persona.nombre,
+  (actual, anterior) => {
+    console.log("Nombre actual:", actual, "Nombre anterior:", anterior);
+  }
+);
 
-    return {
-      text,
-      counter,
-      persona,
-      username,
-      inputUsername,
-      // Computed props
-      nombreCompletoMayus: computed(() => {
-        return `${persona.nombre} ${persona.apellido} de la casa ${casa.value} ${ciudad.value} (${continente.value})`.toUpperCase();
-      }),
-    };
-  },
-};
+// Hooks
+onMounted(() => {
+  inputUsername.value.focus();
+  counter.value = 100;
+  console.log("mounted Composition API");
+});
+
+// Context - Expose. Puede publicar una variable o un objeto
+defineExpose({
+  persona,
+  counter,
+});
 </script>
